6e5f23fcc4554a3131408f365968231279bdab32
princekk122 <awuahkarikari12@gmail.com>
2022-02-08 23:50:10 +0000

    initial commit

---
+++ custom_arrays.py
@@ -0,0 +1,78 @@
+from typing import List, Set
+
+
+class Array:
+    def __init__(self):
+        self.__count = 0
+        self.__items: List[int] = []
+
+    def size(self) -> None:
+        print(self.__count)
+
+    def remove_at(self, index: int) -> None:
+        if index < 0 or index >= self.__count:
+            raise ValueError("Please provide a valid index!")
+
+        for i in range(index, self.__count - 1):
+            self.__items[i] = self.__items[i + 1]
+
+        self.__count -= 1
+
+    def intersect(self, another_array: List[int]) -> List[int]:
+        common_elements: Set = set()
+        self.__items.sort()
+        another_array.sort()
+
+        for i in range(0, self.__count):
+            for j in range(0, len(another_array)):
+                if self.__items[i] < another_array[j]:
+                    break
+
+                if self.__items[i] == another_array[j]:
+                    common_elements.add(self.__items[i])
+
+        return list(common_elements)
+
+    def insert(self, item: int) -> None:
+        self.__count += 1
+        self.__items.append(item)
+
+    def reverse(self) -> List[int]:
+        for i in range(0, self.__count // 2):
+            temp = self.__items[i]
+            self.__items[i] = self.__items[self.__count - 1 - i]
+            self.__items[self.__count - 1 - i] = temp
+
+        return self.__items
+
+    def index_of(self, value: int):
+        for i in range(0, self.__count):
+            if self.__items[i] == value:
+                return i
+
+        return -1
+
+    def max(self) -> int:
+        largest_number = self.__items[0]
+
+        for current_number in self.__items:
+            if current_number > largest_number:
+                largest_number = current_number
+
+        return largest_number
+
+    def insert_at(self, item: int, index: int) -> None:
+        if index < 0 or index >= self.__count:
+            raise ValueError("Please provide a valid index!")
+
+        self.__items.append(0)
+        self.__count += 1
+
+        for i in range(self.__count - 2, index - 1, -1):
+            self.__items[i + 1] = self.__items[i]
+
+        self.__items[index] = item
+
+    def print(self) -> None:
+        for i in range(0, self.__count):
+            print(self.__items[i])
---
+++ main.py
@@ -0,0 +1,10 @@
+from custom_arrays import Array
+
+arr = Array()
+
+arr.insert(-1)
+arr.insert(-5)
+arr.insert(2)
+arr.insert(10)
+arr.insert_at(9, 0)
+arr.print()
